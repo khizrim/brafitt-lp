bb7ea3da7874aa1c98bb4e3891f360aa3b25c455
Khizri Makhmudov <kh.makhmudov@me.com>
2021-10-27 18:02:17 +0300

    feat: add description to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,13 +3,16 @@ import { Link } from 'react-router-dom';
 
 import './Header.css';
 
-import logo from '../../images/logo.svg';
+import logo from '../../images/logo-v.svg';
 
 const Header = () => (
   <header className="header">
     <Link className="header__logo-link" to="/">
       <img className="header__logo" src={logo} alt="Brafitt" />
     </Link>
+    <p className="header__description">
+      Первый брафиттинг <br /> в Дагестане
+    </p>
   </header>
 );
 
